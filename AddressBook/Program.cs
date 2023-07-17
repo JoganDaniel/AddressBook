b0ca8d38fb149e91d0d732436289b6b78c1db611
Jogan Daniel A <jogan.d@tvsnext.com>
2023-07-17 10:21:55 +0530

    [jogan] Add : Folder Structure implemented

---
+++ AddressBook/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AddressBook
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book program");
+        }
+    }
+}
