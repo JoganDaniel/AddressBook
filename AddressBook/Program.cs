8b7176a9a3d5c8ea5074a7705e3421a1a42bbb99
Jogan Daniel A <jogan.d@tvsnext.com>
2023-07-17 10:41:53 +0530

    [jogan] Add : Ability to create contact

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -6,6 +6,8 @@ namespace AddressBook
         private static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book program");
+            AddressBook addressBook = new AddressBook();
+            addressBook.CreateContact();
         }
     }
 }
