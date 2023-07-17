e781f8b8b7ef0d8326702642dfaa09f775dc8100
Jogan Daniel A <jogan.d@tvsnext.com>
2023-07-17 11:19:42 +0530

    [jogan] Add : Ability to edit contacts

--- AddressBook/AddressBook.cs
+++ AddressBook/AddressBook.cs
@@ -11,7 +11,7 @@ namespace AddressBook
         List<Contact> addressbooklist = new List<Contact>();
         public void CreateContact()
         {
-            Console.WriteLine("Enter the details 1.First Name\n2.Last Name\n3.Address \n4.City Name \n5.State Name \n.6.Zip code \n7.Phone Number \n8.Email Address ");
+            Console.WriteLine("Enter the details\n1.First Name\n2.Last Name\n3.Address \n4.City Name \n5.State Name \n6.Zip code \n7.Phone Number \n8.Email Address ");
             Contact contact = new Contact()
             {
                 FirstName = Console.ReadLine(),
@@ -23,8 +23,58 @@ namespace AddressBook
                 PhoneNumber = Convert.ToInt32(Console.ReadLine()),
                 Email = Console.ReadLine(),
             };
-            Console.WriteLine(contact.FirstName + "\n " + contact.LastName + "\n " + contact.Address + "\n " + contact.City + "\n " + contact.State + "\n " + contact.Zip + "\n " + contact.PhoneNumber + "\n " + contact.Email);
+            Console.WriteLine(contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.Zip + "\n" + contact.PhoneNumber + "\n" + contact.Email);
             addressbooklist.Add(contact);
+        }
+        public void EditContact(string name) 
+        { 
+            foreach(Contact contact in addressbooklist)
+            {
+                if(contact.FirstName == name || contact.LastName.Equals(name))
+                {
+                    Console.WriteLine("Enter what to edit 1.Last Name\n2.Address \n3.City Name \n4.State Name \n5.Zip code \n6.Phone Number \n7.Email Address ");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch(option)
+                    {
+                        case 1:
+                            contact.LastName = Console.ReadLine();
+                            break;
+                        case 2:
+                            contact.Address = Console.ReadLine();
+                            break;
+                        case 3:
+                            contact.City = Console.ReadLine();
+                            break;
+                        case 4:
+                            contact.State = Console.ReadLine();
+                            break;
+                        case 5:
+                            contact.Zip = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 6:
+                            contact.PhoneNumber = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 7:
+                            contact.Email = Console.ReadLine();
+                            break;
+                        default:
+                            break;
+                    }
+                    Console.WriteLine("Edited Details");
+                    Console.WriteLine(contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.Zip + "\n" + contact.PhoneNumber + "\n" + contact.Email);
+                }
+            }
+        }
+
+        public void DisplayContacts()
+        {
+            int c = 1;
+            foreach(Contact contact in addressbooklist)
+            {
+                Console.WriteLine("Contact "+c+":");
+                Console.WriteLine(contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.Zip + "\n" + contact.PhoneNumber + "\n" + contact.Email);
+                c++;
+            }
         }
     }
 }
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -7,7 +7,34 @@ namespace AddressBook
         {
             Console.WriteLine("Welcome to Address Book program");
             Addressbook addressbook = new Addressbook();
-            addressbook.CreateContact();
+
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1.Create Contact\n2.Edit Contact\n3.Display Contacts\n4.Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        addressbook.CreateContact();
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the first name");
+                        string fname = Console.ReadLine();
+                        addressbook.EditContact(fname);
+                        break;
+                    case 3:
+                        addressbook.DisplayContacts();
+                        break;
+                    case 4:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Enter correct choice");
+                        break;
+                }
+            
+            }
         }
     }
 }
