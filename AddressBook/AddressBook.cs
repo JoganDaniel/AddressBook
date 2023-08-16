5e9bb66c5f17285da343a052b9568a0fbaa3fbd8
Jogan Daniel A <jogan.d@tvsnext.com>
2023-08-16 15:20:59 +0530

    [jogan] Search City and State with dictionary

--- AddressBook/AddressBook.cs
+++ AddressBook/AddressBook.cs
@@ -11,9 +11,10 @@ namespace AddressBook
     public class Addressbook
     {
         List<Contact> addressbooklist = new List<Contact>();
-        Dictionary<string,List<Contact>> dict = new Dictionary<string,List<Contact>>();
-        Dictionary<string, List<Contact>> stateDict = new Dictionary<string, List<Contact>>();
-        Dictionary<string, List<Contact>> cityDict = new Dictionary<string, List<Contact>>();
+        public Dictionary<string, List<Contact>> dict = new Dictionary<string, List<Contact>>();
+        public Dictionary<string, List<Contact>> stateDict = new Dictionary<string, List<Contact>>();
+        public Dictionary<string, List<Contact>> cityDict = new Dictionary<string, List<Contact>>();
+        int stateCount, cityCount;
         public void CreateContact()
         {
             Console.WriteLine("Enter the details\n1.First Name\n2.Last Name\n3.Address \n4.City Name \n5.State Name \n6.Zip code \n7.Phone Number \n8.Email Address ");
@@ -27,7 +28,7 @@ namespace AddressBook
                 Zip = Convert.ToInt32(Console.ReadLine()),
                 PhoneNumber = Convert.ToInt32(Console.ReadLine()),
                 Email = Console.ReadLine(),
-            };    
+            };
             if (CheckName(contact))
             {
                 Console.WriteLine("Name is already present");
@@ -46,12 +47,12 @@ namespace AddressBook
             dict.Add(uniquename, addressbooklist);
             addressbooklist = new List<Contact>();
         }
-        public void EditContact(string name,string contactname) 
-        { 
-            foreach(var data in dict)
+        public void EditContact(string name, string contactname)
+        {
+            foreach (var data in dict)
             {
                 if (data.Key.Equals(name))
-                    {
+                {
                     foreach (Contact contact in data.Value)
                     {
                         if (contact.FirstName.Equals(contactname) || contact.LastName.Equals(contactname))
@@ -97,7 +98,8 @@ namespace AddressBook
             Contact contact = new Contact();
             foreach (var item in dict)
             {
-                if (item.Key.Equals(name)) {
+                if (item.Key.Equals(name))
+                {
                     foreach (var data in item.Value)
                     {
                         if (data.FirstName.Equals(contactname) || data.LastName.Equals(contactname))
@@ -138,46 +140,118 @@ namespace AddressBook
         public bool CheckName(Contact contact)
         {
             string name = contact.FirstName;
-            List<Contact> list2 = null;
-            foreach(var data in dict)
+            foreach (var data in dict)
             {
-               list2 = data.Value.Where(x => x.FirstName.Equals(name)).ToList();
+                foreach (var item in data.Value)
+                {
+                    if (item.FirstName.Equals(name))
+                    {
+                        return true;
+                    }
+                }
             }
-            if (list2 == null)
+            return false;
+        }
+
+
+        public void SearchByState()
+        {
+            foreach (var data in dict.Values)
             {
-                return false;
+                foreach (var item in data)
+                {
+                    if (!stateDict.Keys.Equals(item.State) && !stateDict.ContainsKey(item.State))
+                    {
+                        List<Contact> list = new List<Contact>();
+                        list.Add(item);
+                        stateDict.Add(item.State, list);
+                    }
+                    else
+                    {
+                        foreach (var states in stateDict)
+                        {
+                            if (states.Key.Equals(item.State))
+                            {
+                                states.Value.Add(item);
+                            }
+                        }
+                    }
+
+                }
             }
-            return true;
+        }
+
+        public void SearchByCity()
+        {
+            foreach (var data in dict.Values)
+            {
+                foreach (var item in data)
+                {
+                    if (!cityDict.Keys.Equals(item.City))
+                    {
+                        List<Contact> list = new List<Contact>();
+                        list.Add(item);
+                        cityDict.Add(item.City, list);
+                    }
+                    else
+                    {
+                        foreach (var cities in cityDict)
+                        {
+                            if (cities.Key.Equals(item.City))
+                            {
+                                cities.Value.Add(item);
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         public void GetDetailsFromState(string input)
         {
-            List<Contact> result = null;
-            foreach (var data in dict)
+            stateCount = 0;
+            foreach (var data in stateDict)
             {
-                result = data.Value.Where(x => x.State.Equals(input)).ToList();
+                if (data.Key.Equals(input))
+                {
+                    Console.WriteLine("State : " + data.Key);
+                    var slist = stateDict.GetValueOrDefault(data.Key);
+                    DisplayList(slist.ToList());
+                    stateCount = slist.Count;
+                }
             }
-                stateDict.Add(input, result);
-                DisplayDict(stateDict);
-       
         }
         public void GetDetailsFromCity(string input)
         {
-            List<Contact> result = null;
-            foreach (var data in dict)
+            cityCount = 0;
+            foreach (var data in cityDict)
             {
-                result = data.Value.Where(x => x.City.Equals(input)).ToList();
+                if (data.Key.Equals(input))
+                {
+                    Console.WriteLine("City : " + data.Key);
+                    var clist = cityDict.GetValueOrDefault(data.Key);
+                    DisplayList(clist.ToList());
+                    cityCount = clist.Count;
+                }
             }
-            
-                cityDict.Add(input, result);
-                DisplayDict(cityDict);
-            
         }
-        public void DisplayDict(Dictionary<string,List<Contact>> dict)
+        public void GetContactCountFromState(string input)
+        {
+            GetDetailsFromState(input);
+            Console.WriteLine("Count : " + stateCount);
+
+        }
+        public void GetContactCountFromCity(string input)
+        {
+            GetDetailsFromCity(input);
+            Console.WriteLine("Count : " + cityCount);
+
+        }
+        public void DisplayDict(Dictionary<string, List<Contact>> dict)
         {
             foreach (var data in dict)
             {
-                Console.WriteLine("Key : "+data.Key);
+                Console.WriteLine("Key : " + data.Key);
                 foreach (var contact in data.Value)
                 {
                     Console.WriteLine(contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.Zip + "\n" + contact.PhoneNumber + "\n" + contact.Email);
@@ -190,6 +264,7 @@ namespace AddressBook
             foreach (var contact in list)
             {
                 Console.WriteLine(contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.Zip + "\n" + contact.PhoneNumber + "\n" + contact.Email);
+                Console.WriteLine();
             }
         }
 
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -14,7 +14,7 @@ namespace AddressBook
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("1.Create Contact\n2.Add to Dictionary\n3.Edit Contact\n4.Display Contacts\n5.Delete Contact\n6.Serialize dict to JSON\n7.Search From state using dictionary\n8.Search From cityusing dictionary\n9.Exit");
+                Console.WriteLine("1.Create Contact\n2.Add to Dictionary\n3.Edit Contact\n4.Display Contacts\n5.Delete Contact\n6.Serialize dict to JSON\n7.Search From state using dictionary\n8.Search From city using dictionary\n9.Number of contact in state\n10.Number of contact in city\n11.AddToCityAndStateDictionary\n12.Exit");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 string key;
                 switch (choice)
@@ -27,10 +27,10 @@ namespace AddressBook
                         break;
                     case 3:
                         Console.WriteLine("Enter key");
-                         key = Console.ReadLine();
+                        key = Console.ReadLine();
                         Console.WriteLine("Enter the first name");
                         string fname = Console.ReadLine();
-                        addressbook.EditContact(key,fname);
+                        addressbook.EditContact(key, fname);
                         break;
                     case 4:
                         addressbook.DisplayContacts();
@@ -40,7 +40,7 @@ namespace AddressBook
                         key = Console.ReadLine();
                         Console.WriteLine("Enter name");
                         string name = Console.ReadLine();
-                        addressbook.DeleteContact(key,name);
+                        addressbook.DeleteContact(key, name);
                         break;
                     case 6:
                         addressbook.WriteToJsonFile(file_path);
@@ -56,13 +56,27 @@ namespace AddressBook
                         addressbook.GetDetailsFromCity(city);
                         break;
                     case 9:
+                        Console.WriteLine("Enter state name");
+                        string statename = Console.ReadLine();
+                        addressbook.GetContactCountFromState(statename);
+                        break;
+                    case 10:
+                        Console.WriteLine("Enter city");
+                        string cityname = Console.ReadLine();
+                        addressbook.GetContactCountFromCity(cityname);
+                        break;
+                    case 11:
+                        addressbook.SearchByCity();
+                        addressbook.SearchByState();
+                        break;
+                    case 12:
                         flag = false;
                         break;
                     default:
                         Console.WriteLine("Enter correct choice");
                         break;
                 }
-            
+
             }
         }
     }
